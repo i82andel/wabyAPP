ecd93b9def3e9342fafba6198ea89de07e5b0f42
Aneri00 <48674592+i82andel@users.noreply.github.com>
2021-11-29 16:54:56 +0100

    spot home prueba

--- app/src/main/java/com/racoon/waby/data/repository/SpotRepository.kt
+++ app/src/main/java/com/racoon/waby/data/repository/SpotRepository.kt
@@ -4,79 +4,82 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.racoon.waby.data.model.Spot
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
-class SpotRepository @Inject constructor(private val spotList: CollectionReference) {
-    class SpotRepository @Inject constructor(private val spotList: CollectionReference) {
 
-        fun addNewSpot(spot: Spot) {
-            try {
-                spotList.document(spot.idSpot).set(spot)
-            } catch (e: Exception) {
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+class SpotRepository{
+
+    private val fireste = Firebase.firestore
+    private val spotList = fireste.collection("Spot")
+    fun addNewSpot(spot: Spot) {
+        try {
+            spotList.document(spot.idSpot).set(spot)
+        } catch (e: Exception) {
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-
-        fun getAllSpots():LiveData<MutableList<Spot>>{
-            val mutableList = MutableLiveData<MutableList<Spot>>()
-            spotList.get().addOnSuccessListener {
-                val spotDataList = mutableListOf<Spot>()
-                for (document in it){
-                    val spot = document.toObject(Spot::class.java)
-                    spotDataList.add(spot)
-                }
-                mutableList.value = spotDataList
-            }
-            return mutableList
-        }
-
-        fun getSingleSpot(idSpot: String): Spot {
-
-            var spot= Spot("not_found")
-            spotList
-            val docRef = spotList.document(idSpot)
-                .get()
-                .addOnSuccessListener { document ->
-                    if (document.exists()) {
-
-                        spot = document.toObject(Spot::class.java)!!
-                        /*val adminUser = document.getString("adminUser")
-                        val badges = document.get("badges") to ArrayList<String>()
-                        val capacity = document.getLong("capacity")?.toInt()
-                        val description = document.get("description").toString()
-                        val images = document.get("images") to ArrayList<String>()
-                        val location = document.getString("location")
-                        val name = document.get("name").toString()
-                        val phoneNumber = document.get("phoneNumber").toString()
-                        val spotType = document.getString("spotType")
-                        val website = document.getString("website")
-                        val ratings = document.get("ratings") to ArrayList<Int>()
-
-                        val spot = Spot(
-                            idSpot,
-                            name,
-                            adminUser,
-                            phoneNumber,
-                            capacity,
-                            location,
-                            ratings,
-                            website,
-                            spotType,
-                            badges,
-                            images,
-                            description
-                        )*/
-
-                    }
-                }
-            return spot
-        }
-
-
     }
+
+    fun getAllSpots():LiveData<MutableList<Spot>>{
+        val mutableList = MutableLiveData<MutableList<Spot>>()
+        spotList.get().addOnSuccessListener {
+            val spotDataList = mutableListOf<Spot>()
+            for (document in it){
+                val spot = document.toObject(Spot::class.java)
+                spotDataList.add(spot)
+            }
+            mutableList.value = spotDataList
+        }
+        return mutableList
+    }
+
+    fun getSingleSpot(idSpot: String): Spot {
+
+        var spot= Spot("not_found")
+        spotList
+        val docRef = spotList.document(idSpot)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+
+                    spot = document.toObject(Spot::class.java)!!
+                    /*val adminUser = document.getString("adminUser")
+                    val badges = document.get("badges") to ArrayList<String>()
+                    val capacity = document.getLong("capacity")?.toInt()
+                    val description = document.get("description").toString()
+                    val images = document.get("images") to ArrayList<String>()
+                    val location = document.getString("location")
+                    val name = document.get("name").toString()
+                    val phoneNumber = document.get("phoneNumber").toString()
+                    val spotType = document.getString("spotType")
+                    val website = document.getString("website")
+                    val ratings = document.get("ratings") to ArrayList<Int>()
+
+                    val spot = Spot(
+                        idSpot,
+                        name,
+                        adminUser,
+                        phoneNumber,
+                        capacity,
+                        location,
+                        ratings,
+                        website,
+                        spotType,
+                        badges,
+                        images,
+                        description
+                    )*/
+
+                }
+            }
+        return spot
+    }
+
 }
--- app/src/main/java/com/racoon/waby/ui/spothome/SpotHomeFragment.kt
+++ app/src/main/java/com/racoon/waby/ui/spothome/SpotHomeFragment.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.racoon.waby.data.model.Spot
+import com.racoon.waby.data.repository.SpotRepository
 import com.racoon.waby.databinding.FragmentSpotHomeBinding
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class SpotHomeFragment : Fragment() {
+@Singleton
+class SpotHomeFragment @Inject constructor ( spotHomeViewModel: SpotHomeViewModel) : Fragment() {
 
     private lateinit var spotHomeViewModel: SpotHomeViewModel
     private var _binding: FragmentSpotHomeBinding? = null
+    private lateinit var adapter:MySpotAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,17 +38,23 @@ class SpotHomeFragment : Fragment() {
         _binding = FragmentSpotHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        /*val textView: TextView = binding.textSpotHome
-        spotHomeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        adapter = MySpotAdapter(context)
+        binding.spotList.layoutManager = LinearLayoutManager(context)
+        binding.spotList.adapter = adapter
 
+    }
+
+    fun observeData(){
+        spotHomeViewModel.getSpotList().observe(this, Observer {
+            adapter.setSpotList(it)
+            adapter.notifyDataSetChanged()
+        })
     }
 
     override fun onDestroyView() {
