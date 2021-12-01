be99271476e3da3a6f0bbcc72a85a8a562a9f086
Cristian1711 <i82goorc@uco.es>
2021-12-01 23:15:40 +0100

    no lee bien, solo abre la app cuando detecta un nfc

--- app/src/main/java/com/racoon/waby/ui/home/main/MainHomeFragment.kt
+++ app/src/main/java/com/racoon/waby/ui/home/main/MainHomeFragment.kt
@@ -2,7 +2,10 @@ package com.racoon.waby.ui.home.main
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.Intent.getIntent
+import android.content.Intent.parseUri
 import android.content.IntentFilter
+import android.nfc.NdefMessage
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,9 +18,16 @@ import com.racoon.waby.R
 import com.racoon.waby.databinding.FragmentMainHomeBinding
 import com.racoon.waby.ui.home.map.MapActivity
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.os.Parcelable
 import android.provider.Settings
+import android.util.Log
 import com.racoon.waby.ui.auxiliarActivity
 import com.racoon.waby.ui.spot.SpotActivity
+import java.io.UnsupportedEncodingException
+import java.nio.charset.Charset
+import kotlin.experimental.and
+import android.app.Activity
 
 
 class MainHomeFragment : Fragment() {
@@ -33,6 +43,9 @@ class MainHomeFragment : Fragment() {
     }
 
     private var pendingIntent:PendingIntent? = null
+
+    private var myTag:Tag? = null
+
 
     //viewModel
     private val viewModel by viewModels<MainHomeViewModel>()
@@ -72,6 +85,7 @@ class MainHomeFragment : Fragment() {
             }
             else{
                 Toast.makeText(context,R.string.success_nfc,Toast.LENGTH_SHORT).show()
+
             }
         }
     }
@@ -83,11 +97,56 @@ class MainHomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        readFromIntent(requireActivity().intent)
         pendingIntent = PendingIntent.getActivity(
             context,0,Intent(context, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0
         )
 
-        
-        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        val tagDetected = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT)
+
     }
+
+    private fun readFromIntent(intent: Intent){
+        var action:String? = intent.action
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action
+            || NfcAdapter.ACTION_TECH_DISCOVERED == action
+            || NfcAdapter.ACTION_NDEF_DISCOVERED == action
+        ){
+            var rawMsgs:Array<Parcelable>? = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            var msgs: Array<NdefMessage?>? = null
+
+            if (rawMsgs != null){
+
+                msgs = arrayOfNulls(rawMsgs.size)
+
+                for (i in rawMsgs.indices) {
+                    msgs[i] = rawMsgs[i] as NdefMessage
+                }
+            }
+            buildTagViews(msgs)
+        }
+    }
+
+    private fun buildTagViews(msgs: Array<NdefMessage?>?) {
+        if (msgs == null || msgs.isEmpty()) return
+        var text = ""
+        val payload = msgs[0]!!.records[0].payload
+        val UTF_16: Charset = Charset.forName("UTF-16")
+        val languageCodeLength: Byte = payload[0] and 51
+        try {
+            text = String(
+                payload,
+                languageCodeLength + 1,
+                payload.size - languageCodeLength - 1,
+                UTF_16
+            )
+        } catch (e: UnsupportedEncodingException) {
+            Log.e("Unsupported", e.toString())
+        }
+
+        binding.textView6.setText("Contenido NFC:" + text)
+    }
+
 }
