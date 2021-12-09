a1bcc463c7166d40229690b6730610fb967fefd3
Cristian1711 <i82goorc@uco.es>
2021-12-09 19:09:00 +0100

    swipe

--- app/src/main/java/com/racoon/waby/ui/spot/swipe/SwipeFragment.kt
+++ app/src/main/java/com/racoon/waby/ui/spot/swipe/SwipeFragment.kt
@@ -1,19 +1,24 @@
 package com.racoon.waby.ui.spot.swipe
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import com.racoon.waby.R
 import com.racoon.waby.databinding.FragmentSwipeBinding
-import com.racoon.waby.ui.spot.notifications.NotificationsViewModel
 
 class SwipeFragment : Fragment(){
 
     private lateinit var swipeViewModel: SwipeViewModel
     private var _binding: FragmentSwipeBinding? = null
+    private var arrayAdapter: arrayAdapter? = null
+    private var rowItems: MutableList<cards>? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,6 +36,50 @@ class SwipeFragment : Fragment(){
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var card_1 = cards("crid","manuel","https://i.pinimg.com/474x/d3/65/50/d36550ea1b17394f63c2dff243fe636d.jpg")
+        var card_2 = cards("au","adios","https://i.pinimg.com/474x/d3/65/50/d36550ea1b17394f63c2dff243fe636d.jpg")
+
+        rowItems = arrayListOf(card_1,card_2)
+
+        arrayAdapter = arrayAdapter(context, R.layout.item, rowItems)
+        binding.frame.adapter = arrayAdapter
+
+        binding.frame.setFlingListener(object : SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                Log.d("LIST", "removed object!")
+                rowItems?.removeAt(0)
+                arrayAdapter!!.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(dataObject: Any) {
+                val obj = dataObject as cards
+                val userId = obj.userId
+                //usersDb?.child(userId)?.child("connections")?.child("nope")?.child(currentUId)?.setValue(true)
+                Toast.makeText(requireContext(), "Left", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(dataObject: Any) {
+                val obj = dataObject as cards
+                val userId = obj.userId
+                //usersDb?.child(userId)?.child("connections")?.child("yeps")?.child(currentUId)?.setValue(true)
+                //isConnectionMatch(userId)
+                Toast.makeText(requireContext(), "Right", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {}
+            override fun onScroll(scrollProgressPercent: Float) {}
+        })
+
+        binding.likeButton.setOnClickListener{
+            rowItems?.removeAt(0)
+            arrayAdapter!!.notifyDataSetChanged()
+        }
+
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/racoon/waby/ui/spot/swipe/cards.kt
@@ -0,0 +1,9 @@
+package com.racoon.waby.ui.spot.swipe
+
+class cards(var userId: String, var name: String, var profileImageUrl: String) {
+
+    fun setUserID(userID: String?) {
+        userId = userId
+    }
+
+}
