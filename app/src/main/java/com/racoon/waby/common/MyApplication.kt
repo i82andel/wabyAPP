7a3d0ac3020f37e5749af26899d730ef11dfdcfc
yunesss00 <younessbb00@gmail.com>
2021-12-12 13:27:20 +0100

    push

--- app/src/main/java/com/racoon/waby/common/MyApplication.kt
+++ app/src/main/java/com/racoon/waby/common/MyApplication.kt
@@ -4,13 +4,23 @@ import android.app.Application
 import com.racoon.waby.R
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.client.notifications.handler.NotificationConfig
 import io.getstream.chat.android.livedata.ChatDomain
+import io.getstream.chat.android.pushprovider.firebase.FirebasePushDeviceGenerator
 
 class MyApplication : Application(){
     override fun onCreate() {
         super.onCreate()
+        
+        val notificationConfig = NotificationConfig(
+            pushDeviceGenerators = listOf(FirebasePushDeviceGenerator())
+        )
+
         val client =
-            ChatClient.Builder(getString(R.string.api_key), this).logLevel(ChatLogLevel.ALL).build()
+            ChatClient.Builder(getString(R.string.api_key), this)
+                .logLevel(ChatLogLevel.ALL)
+                .notifications(notificationConfig)
+                .build()
         ChatDomain.Builder(client, this).build()
     }
 }
