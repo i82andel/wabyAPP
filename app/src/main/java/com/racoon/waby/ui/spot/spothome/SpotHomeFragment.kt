b8174aa9f798eedd306841773c97dd3bf005a1cb
Aneri00 <48674592+i82andel@users.noreply.github.com>
2021-12-14 17:07:23 +0100

    tags

--- app/src/main/java/com/racoon/waby/ui/spot/spothome/SpotHomeFragment.kt
+++ app/src/main/java/com/racoon/waby/ui/spot/spothome/SpotHomeFragment.kt
@@ -10,6 +10,7 @@ import android.widget.LinearLayout.HORIZONTAL
 import android.widget.RatingBar.OnRatingBarChangeListener
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -35,6 +36,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.internal.wait
+import javax.annotation.meta.When
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.math.roundToInt
@@ -77,8 +79,38 @@ class SpotHomeFragment : Fragment() {
             binding.description.text = spotFinal.description
             binding.website.text = spotFinal.website
             spotHomeViewModel.addAssistant(idSpot, userId)
+            setTags(spotFinal.badges)
         }
 
+
+    }
+
+    private fun setTags(badges: ArrayList<String>?) {
+        var countNumber = 0
+        if (badges != null) {
+            for (badge in badges){
+
+                when(countNumber){
+
+                    0 -> {binding.tag1.text = badge
+                        binding.Tag1.visibility = View.VISIBLE
+                    }
+                    1 -> {binding.tag2.text = badge
+                        binding.Tag2.visibility = View.VISIBLE
+                    }
+                    2 ->{binding.tag3.text = badge
+                        binding.Tag3.visibility = View.VISIBLE
+                    }
+                    3 -> {binding.tag4.text = badge
+                        binding.Tag4.visibility = View.VISIBLE
+                    }
+                    4 -> {binding.tag5.text = badge
+                        binding.Tag5.visibility = View.VISIBLE
+                    }
+                }
+                countNumber++
+            }
+        }
 
     }
 
