797d9b504a9eae91fdad7c069cd6b5fb3b347ccb
Aneri00 <48674592+i82andel@users.noreply.github.com>
2021-12-15 19:49:53 +0100

    wabi

--- app/src/main/java/com/racoon/waby/ui/spot/wabis/WabisFragment.kt
+++ app/src/main/java/com/racoon/waby/ui/spot/wabis/WabisFragment.kt
@@ -5,21 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.racoon.waby.databinding.FragmentSpotHomeBinding
 import com.racoon.waby.databinding.FragmentWabisBinding
-import com.racoon.waby.ui.spot.chat.ChannelActivity
-import com.racoon.waby.ui.spot.spothome.MySpotAdapter
-import com.racoon.waby.ui.spot.spothome.SpotHomeViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlin.system.measureTimeMillis
+
 
 class WabisFragment : Fragment(){
 
